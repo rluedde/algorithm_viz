b03dd6faee9ef5d0e1960ee91cbaa319860b9d05
Rei <rei@git.com>
2020-06-21 14:26:45 -0700

    Rectangles are now printed properly
    Next step is to make a function that produces
    an array with all Book objects from the actual data

--- index.js
+++ index.js
@@ -12,17 +12,21 @@ class Book {
     constructor(length, title, max_len) {
         this.length = length;
         this.height = 20;
-        this.color = this.getBookColor(this.length, max_len)
+        this.color = this.getBookColor(length, max_len)
         this.title = title;
     }
     
     
-    getBookColor(index, max_len) {
+    getBookColor(length, max_len) {
     // return an RGB color on a gradient
     // reds are longer, greens are shorter
     // need maximum book length in order to have a cap
-    // on the color!!!!
-    return [99, 50, 15]
+    // on the color!!!!.
+    let R = Math.floor(length / max_len * 100)
+    let G = Math.floor((1 - (length / max_len)) * 100)
+    let B = 0
+    return [R, G, B]
+
     }
 
 
@@ -37,36 +41,31 @@ class Book {
 }
 
 
-// for displaying all books on the canvas
-// the meaning of this class name is purely symbolic
-class Library {
+let table;
 
-    constructor(bookArray) {
-        this.bookArray = bookArray;
-    }
-
-    show() {
-        // go through each book in bookArray and call Book.show(index)
-        // probably use forEach here
-        for (var i  = 0; i < this.bookArray.length; i++) {
-            let book = this.bookArray[i]
-            book.show(i, book.color, book.length)
-        }
-    }
+function preload() {
+    table = loadTable('assets/book_data.csv', 'csv', 'header');
 }
 
+// these should be global
+var max_len;
+var num_books;
 
 function setup() {
     createCanvas(500, 500);
     background(100)
-    colorMode(RGB, 100)
+    max_len = Math.max(...table.getColumn('pages').map(Number))
+    num_books = table.getRowCount()
+    print(table.getColumnCount());
 }
+
 
 function draw() {
     // the data shouldn't be loaded every time it draws lmao
-    const data = loadTable('assets/book_data.csv', 'csv', 'header')
-    const max_len = max(data.getColumn('pages'))
-    const num_books = data.getRowCount()
-    let b1 = new Book(100, "cows", max_len)
-    b1.show(1)
+    let b1 = new Book(1200, "cows", max_len, num_books)
+    let b2 = new Book(300, "boys", max_len, num_books)
+    let books = [b1, b2]
+    for (var i = 0; i < books.length; i++){
+        books[i].show(i)
+    }
 }
