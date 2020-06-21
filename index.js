8c527d77dc789e40768c85d09f6319c403ed9636
Rei <rei@git.com>
2020-06-21 15:12:31 -0700

    Animate entire bookArray to the screeen
    Remove outliers programmatically (not in the data). I want to potentially
    use a ln transform to incldue the outliers but they would be represented much
    more kindly (at least in my head).

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Book {
 
     constructor(length, title, max_len) {
         this.length = length;
-        this.height = 20;
+        this.height = 10;
         this.color = this.getBookColor(length, max_len)
         this.title = title;
     }
@@ -34,9 +34,10 @@ class Book {
         // probably use this.color as an array and then use the spread op 
         // in fill
         fill(...this.color);
-        rect(30, 30 * (index + 1) + 30, this.length, this.height);
+        noStroke()
+        rect(10, this.height * (index + 1), this.length, this.height);
         // receive what position in the array the book is and 
-        // build a rectangle at that position accordingly
+        // build a rectangle at that position 
     }
 }
 
@@ -50,22 +51,38 @@ function preload() {
 // these should be global
 var max_len;
 var num_books;
+var filterThreshhold = 1000;
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(10000, 5000);
     background(100)
-    max_len = Math.max(...table.getColumn('pages').map(Number))
+    // find the max of all books of length <= 1000 pages (outliers)
+    max_len = Math.max(...table.getColumn('pages').map(Number).filter(
+        num => num <= 1000
+    ))
     num_books = table.getRowCount()
     print(table.getColumnCount());
+    bookArray = getBookArray(table)
+    for (var i = 0; i < bookArray.length; i++){
+        bookArray[i].show(i)
+    }
+}
+
+function getBookArray(table) {
+    let bookArray = []
+    // go through each table row and make a Book
+    for (var r = 0; r < num_books; r++) {
+        pages_r = table.getNum(r, "pages")
+        if (pages_r < filterThreshhold) {
+            title_r = table.getString(r, "title")
+            book_r = new Book(pages_r, title_r, max_len)
+            bookArray.push(book_r)
+        }
+    }
+    return bookArray
 }
 
 
 function draw() {
-    // the data shouldn't be loaded every time it draws lmao
-    let b1 = new Book(1200, "cows", max_len, num_books)
-    let b2 = new Book(300, "boys", max_len, num_books)
-    let books = [b1, b2]
-    for (var i = 0; i < books.length; i++){
-        books[i].show(i)
-    }
+    
 }
