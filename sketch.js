49bbddf837d8d54581cbcc77d5c829a84fa5e4bf
Rei <rei@git.com>
2020-06-22 07:10:03 -0700

    Rename some variables

--- sketch.js
+++ sketch.js
@@ -4,21 +4,21 @@
 // https://stackoverflow.com/questions/1069666/sorting-object-property-by-values
 
 let table;
-let max_len;
-let num_books;
+let maxLen;
+let numBooks;
 let filterThreshhold = 1000;
 
 function preload() {
     table = loadTable('assets/book_data.csv', 'csv', 'header');
 }
 
-function getBookArray(table) {
+function getBookArray(table, numBooks, filterThreshhold, maxLen) {
     let bookArray = []
-    for (var r = 0; r < num_books; r++) {
+    for (var r = 0; r < numBooks; r++) {
         pages_r = table.getNum(r, "pages")
         if (pages_r < filterThreshhold) {
             title_r = table.getString(r, "title")
-            book_r = new Book(pages_r, title_r, max_len)
+            book_r = new Book(pages_r, title_r, maxLen)
             bookArray.push(book_r)
         }
     }
@@ -28,17 +28,20 @@ function getBookArray(table) {
 function setup() {
     createCanvas(1000, 1000);
     background(100)
-    // find the max of all books of length <= 1000 pages (outliers)
-    max_len = Math.max(...table.getColumn('pages').map(Number).filter(
-        num => num <= 1000
-    ))
-    num_books = table.getRowCount()
-    bookArray = getBookArray(table)
-    for (var i = 0; i < bookArray.length; i++){
-        bookArray[i].show(i)
-    }
+
 }
 
 
 function draw() {
+    noLoop()
+    // find the max of all books of length <= 1000 pages (outliers)
+    maxLen = Math.max(...table.getColumn('pages').map(Number).filter(
+        num => num <= 1000
+    ))
+    numBooks = table.getRowCount()
+    bookArray = getBookArray(table, numBooks, filterThreshhold, maxLen)
+    for (var i = 0; i < bookArray.length; i++){
+        bookArray[i].show(i)
+    }
+    redraw()
 }
